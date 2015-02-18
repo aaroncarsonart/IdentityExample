ebee04fc419739a90a708069df680deb5a89e12d
Jared Petersen <jpetersen11@wou.edu>
2015-02-17 20:10:57 -0800

    Added IdentityExample Project Code

---
+++ IdentityExample/Controllers/ReviewsController.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using IdentityExample.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace IdentityExample.Controllers
+{
+    public class ReviewsController : Controller
+    {
+        private ApplicationDbContext db;
+        private UserManager<ApplicationUser> manager;
+
+        public ReviewsController()
+        {
+            db = new ApplicationDbContext();
+            manager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(db));
+        }
+
+        // GET: Reviews
+        public async Task<ActionResult> Index()
+        {            
+            return View(await db.Reviews.ToListAsync());
+        }
+
+        // GET: Reviews/Details/5
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Review review = await db.Reviews.FindAsync(id);
+            if (review == null)
+            {
+                return HttpNotFound();
+            }
+            return View(review);
+        }
+
+        [Authorize]
+        // GET: Reviews/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Reviews/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [Authorize]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "ReviewID,Content,ReviewDate,Score")] Review review)
+        {
+            var currentUser = manager.FindById(User.Identity.GetUserId());
+            
+            if (ModelState.IsValid)
+            {
+                review.User = manager.FindById(User.Identity.GetUserId());
+                db.Reviews.Add(review);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            return View(review);
+        }
+
+        // GET: Reviews/Edit/5
+        [Authorize]
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Review review = await db.Reviews.FindAsync(id);
+            if (review == null)
+            {
+                return HttpNotFound();
+            }
+            return View(review);
+        }
+
+        // POST: Reviews/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [Authorize]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "ReviewID,Content,ReviewDate,Score")] Review review)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(review).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(review);
+        }
+
+        // GET: Reviews/Delete/5
+        [Authorize]
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Review review = await db.Reviews.FindAsync(id);
+            if (review == null)
+            {
+                return HttpNotFound();
+            }
+            return View(review);
+        }
+
+        // POST: Reviews/Delete/5
+        [Authorize]
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            Review review = await db.Reviews.FindAsync(id);
+            db.Reviews.Remove(review);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ IdentityExample/Models/Review.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace IdentityExample.Models
+{
+    public enum Score
+    {
+        Like, Dislike
+    }
+
+    public class Review
+    {
+        [Key]
+        public int ReviewID { get; set; }
+
+        [StringLength(500, ErrorMessage = "Content cannot be longer than 500 characters.")]
+        [DataType(DataType.MultilineText)]
+        [Required]
+        public string Content { get; set; }
+
+        [Display(Name = "Review Date")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:yyyy-MM-dd hh:mm tt}")]
+        public DateTime ReviewDate { get; set; }
+
+        public Score? Score { get; set; }
+
+        public virtual ApplicationUser User { get; set; }
+    }
+}
